58a498399627faf16d737ef7e40f4041b62d94df
TinzarTun <tinzartun2002@gmail.com>
2025-08-08 23:46:15 +0630

    database update. create route and controller.

---
+++ controllers/foundItemController.js
@@ -0,0 +1,33 @@
+const bcrypt = require("bcryptjs");
+const PrismaClient = require("@prisma/client").PrismaClient;
+const prisma = new PrismaClient();
+
+const path = require("path");
+const fs = require("fs");
+
+const VALID_LOCATIONS = [
+  "Downtown Yangon",
+  "North Yangon",
+  "South Yangon",
+  "East Yangon",
+  "West Yangon",
+];
+
+exports.getCreateFoundItem = async (req, res) => {
+  try {
+    const categories = await prisma.categoryType.findMany();
+
+    res.render("post/found/create", {
+      title: "Report Found Item",
+      error: null,
+      categories,
+    });
+  } catch (err) {
+    console.error("Error loading categories:", err);
+    res.render("post/found/create", {
+      title: "Report Found Item",
+      error: "Failed to load categories.",
+      categories: [], // Fallback so EJS doesn’t crash
+    });
+  }
+};
